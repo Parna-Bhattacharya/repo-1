a4b80a7102304b8b1b59420a5cdc51df58e96ec9
Parna-Bhattacharya <bhattacharyaparna54@gmail.com>
2025-12-04 15:55:00 +0530

    5th commit

--- Amazon_project/news/All_click.py
+++ Amazon_project/news/All_click.py
@@ -1,38 +1,3 @@
-# import logging
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# import time
-#
-# class AllClick:
-#     def __init__(self, driver):
-#         self.driver = driver
-#         self.all = (By.XPATH, "//a[@id='nav-hamburger-menu']")
-#         self.wait=WebDriverWait(self.driver,10)
-#
-#
-#
-#     def news(self):
-#         self.driver.find_element(*self.all).click()
-#
-#         # Wait for Fire TV anchor tag to actually be clickable
-#         fire_tv = self.wait.until(
-#             EC.element_to_be_clickable((By.XPATH, "//a[normalize-space()='Echo & Alexa']"))
-#         )
-#
-#         # Scroll into view (important for Amazon)
-#         self.driver.execute_script("arguments[0].scrollIntoView(true);", fire_tv)
-#         time.sleep(1)
-#
-#         fire_tv.click()
-#         time.sleep(5)
-#
-#     # def echo(self):
-#     #     alexa=self.driver.find_elements(By.XPATH,"//section[@aria-labelledby='Content & Resources']//ul/li")
-#     #     for i in alexa:
-#     #         if i.text == "Alexa Skills":
-#     #             i.click()
-#
 
 
 import logging
